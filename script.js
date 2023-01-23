abae6a30f773f697cd0b45f453ee715bc65d8996
Xan <alex.phillipspaquet@gmail.com>
2023-01-23 11:53:50 -0500

    Etchasketch

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 const slider = document.getElementById("slider");
 const selector = document.getElementById("selector");
+const selectValue = document.getElementById("selectValue");
+
+selectValue.innerHTML = slider.value;
 
 slider.oninput = function(){
+    selectValue.innerHTML = this.value;
     selector.style.left = this.value + "%";
+
+    // Altered Traits - Meditation book
 }
+
+let buttonLog = document.querySelector("button");
+
+buttonLog.addEventListener('click', function getRange(){
+    let num = document.getElementById("slider").value;
+    console.log(num);
+}
+    )
