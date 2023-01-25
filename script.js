92008a0b866806ff961c5b1848f92d6778a1b8ea
Xan <alex.phillipspaquet@gmail.com>
2023-01-25 08:04:26 -0500

    Etchasketch

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const slider = document.getElementById("slider");
 const selector = document.getElementById("selector");
 const selectValue = document.getElementById("selectValue");
 
+const grid = document.querySelector('#grid');
+const addTile = document.createElement('div');
+    addTile.classList.add('tile');
+
+
 selectValue.innerHTML = slider.value;
 
 slider.oninput = function(){
@@ -16,5 +21,8 @@ let buttonLog = document.querySelector("#submit");
 buttonLog.addEventListener('click', function getRange(){
     let num = document.getElementById("slider").value;
     console.log(num);
-}
-    )
+    grid.appendChild(addTile);
+});
+
+
+
